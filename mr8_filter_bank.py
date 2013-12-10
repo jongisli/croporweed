2f1d712df9e380565b7de70ddf0bbda174b75dbd
jongisli <jongisli@gmail.com>
2013-12-10 14:26:20 +0100

    Added edge filters

--- mr8_filter_bank.py
+++ mr8_filter_bank.py
@@ -6,13 +6,34 @@ from math import cos, sin, pi
 
 def generate_filters():
     filters = {}
-    # scales for edge and bar filters
+    # scales and orientations for edge and bar filters
     scales = [(1,3), (2,6), (4,12)]
+    orientations = [pi/6, pi/3, pi/2, 2*pi/3, 5*pi/6]
     
     # Gaussian filter
-    gauss = lambda x: gaussian_filter(x, 10)
+    gauss_filter = lambda I: gaussian_filter(I, 10)
+    filters.append(gauss_filter)
     # Laplacian of Gaussian filter
-    laplacian = lambda x: gaussian_laplace(x, 10)
-    
-    
+    laplacian_filter = lambda I: gaussian_laplace(I, 10)
+    filters.append(laplacian_filter)
+    # Edge filters (first order Gaussian derivative)
+    for scale in scales:
+        break
+
+    return filters
+
+
+def max_edge_filter(I, scale, angles):
+    sigma_x, sigma_y = scale
+    F_x = gaussian_filter1d(I, sigma_x, axis=-1, order=1)
+    F_y = gaussian_filter1d(I, sigma_y, axis=0, order=1)
+
+    responses = []
+    for angle in angles:
+        response = cos(angle) * F_x + sin(angle) * F_y)
+        responses.append(response)
+
+    aggr_responses = np.dstack(responses)
+    return np.max(A, axis=2)
+        
     
